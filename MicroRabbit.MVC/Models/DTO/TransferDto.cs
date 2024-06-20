52896c770662f9793491a818460f4a7116e673cf
Mykola Lehkyi <mykola.lehkyi@dmtholding.com>
2024-06-20 23:05:52 +0300

    Added MVC project

---
+++ MicroRabbit.MVC/Models/DTO/TransferDto.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MicroRabbit.MVC.Models.DTO
+{
+    public class TransferDto
+    {
+        public int Id { get; set; }
+        public string AccountType { get; set; }
+        public decimal AccountBalance { get; set; }
+    }
+}
